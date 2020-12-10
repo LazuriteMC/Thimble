36002a66887802e0232a06e21392e0b366437df6
Ethan Johnson <ethan@ejohnsons.com>
2020-12-09 18:29:45 -0600

    Documented AttachCompS2C

--- src/main/java/dev/lazurite/thimble/composition/packet/AttachCompS2C.java
+++ src/main/java/dev/lazurite/thimble/composition/packet/AttachCompS2C.java
@@ -18,9 +18,24 @@ import net.minecraft.world.World;
 
 import java.util.stream.Stream;
 
+/**
+ * This packet is used for facilitating {@link Composition}
+ * attachment on the client after being done on the server.
+ * It also handles when an {@link Entity} with a
+ * {@link Composition} attached comes into range of the player.
+ * @author Ethan Johnson
+ */
 public class AttachCompS2C {
     public static final Identifier PACKET_ID = new Identifier(ServerInitializer.MODID, "attach_comp_s2c");
 
+    /**
+     * Handles when the packet is received on the client. First checks if
+     * the given {@link Entity} exists, then checks to make sure the
+     * {@link Composition} is registered. Then the {@link Composition}
+     * is attached.
+     * @param context packet context information
+     * @param buf the contents of the packet
+     */
     public static void accept(PacketContext context, PacketByteBuf buf) {
         World world = context.getPlayer().getEntityWorld();
         int compID = buf.readInt();
@@ -31,12 +46,20 @@ public class AttachCompS2C {
             Composition composition = CompositionRegistry.get(compID);
             Entity entity = world.getEntityById(entityID);
 
-            if (!CompositionTracker.get(entity).contains(composition)) {
-                CompositionTracker.attach(composition, entity);
+            if (entity != null) {
+                if (!CompositionTracker.get(entity).contains(composition)) {
+                    CompositionTracker.attach(composition, entity);
+                }
             }
         });
     }
 
+    /**
+     * This method is called whenever you have a {@link Composition} attached
+     * to a specific {@link Entity} on the server but not the client.
+     * @param composition the {@link Composition} to attach.
+     * @param entity the {@link Entity} to which the {@link Composition} will be attached
+     */
     public static void send(Composition composition, Entity entity) {
         PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
 
@@ -46,12 +69,13 @@ public class AttachCompS2C {
         /* Attached Entity ID */
         buf.writeInt(entity.getEntityId());
 
+        /* Send it! */
         Stream<PlayerEntity> watchingPlayers = PlayerStream.watching(entity.getEntityWorld(), new BlockPos(entity.getPos()));
         watchingPlayers.forEach(player -> ServerSidePacketRegistry.INSTANCE.sendToPlayer(player, PACKET_ID, buf));
     }
 
     /**
-     * Registers the packet in {@link ServerInitializer}.
+     * Registers the packet in {@link dev.lazurite.thimble.side.client.ClientInitializer}.
      */
     public static void register() {
         ClientSidePacketRegistry.INSTANCE.register(PACKET_ID, AttachCompS2C::accept);
