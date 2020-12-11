b64781ebeb2b2a11df66822eb7a399b8b11131de
Ethan Johnson <ethan@ejohnsons.com>
2020-12-10 23:15:29 -0600

    Documented Composition

--- src/main/java/dev/lazurite/thimble/composition/Composition.java
+++ src/main/java/dev/lazurite/thimble/composition/Composition.java
@@ -4,20 +4,49 @@ import dev.lazurite.thimble.synchronizer.Synchronizer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
 
+/**
+ * The main class that you want to extend from. This class allows you
+ * to define custom behavior for any {@link Entity} attached with
+ * a {@link Composition} attached.
+ * @author Ethan Johnson
+ */
 public abstract class Composition {
     private final Synchronizer synchronizer;
 
+    /**
+     * The default constructor. It creates and initializes
+     * the {@link Synchronizer} object.
+     */
     public Composition() {
         this.synchronizer = new Synchronizer();
         this.initSynchronizer();
     }
 
+    /**
+     * @return the {@link Synchronizer} object
+     */
     public Synchronizer getSynchronizer() {
         return this.synchronizer;
     }
 
+    /**
+     * This is where you can define your custom
+     * behavior. An override of this method is
+     * required when you extend this class.
+     * @param entity the entity which this {@link Composition} is attached to
+     */
     public abstract void tick(Entity entity);
+
+    /**
+     * Initializes the {@link Synchronizer}. This is where
+     * you would call {@link Synchronizer#track}.
+     */
     public abstract void initSynchronizer();
+
+    /**
+     * @return the {@link Identifier} object that you define
+     * in your {@link Composition} implementation.
+     */
     public abstract Identifier getIdentifier();
 
     @Override
