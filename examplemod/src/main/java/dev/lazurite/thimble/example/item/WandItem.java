a31683d1ff0ba5361b989548f9f999a01edb440c
Ethan Johnson <ethan@ejohnsons.com>
2020-12-10 19:37:20 -0600

    Documented WandItem

--- examplemod/src/main/java/dev/lazurite/thimble/example/item/WandItem.java
+++ examplemod/src/main/java/dev/lazurite/thimble/example/item/WandItem.java
@@ -18,11 +18,27 @@ import net.minecraft.world.World;
 
 import java.util.List;
 
+/**
+ * This is just meant as a test item for the example compositions.
+ * @author Ethan Johnson
+ */
 public class WandItem extends Item {
+
+    /**
+     * The default constructor.
+     * @param settings the item settings
+     */
     public WandItem(Settings settings) {
         super(settings);
     }
 
+    /**
+     * This method handles whenever a player right-clicks while holding this item.
+     * @param world the world the item is in
+     * @param user the player who right-clicked
+     * @param hand the hand of the player who right-clicked
+     * @return the action result (success, fail, etc.)
+     */
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
@@ -30,14 +46,19 @@ public class WandItem extends Item {
 
         if (world.isClient()) {
             if (hitResult.getType() == HitResult.Type.MISS) {
-                user.playSound(SoundEvents.EVENT_RAID_HORN, 1.0f, 1.0f);
+
+                /* Play a sound when the player right-clicks */
+                user.playSound(SoundEvents.BLOCK_ANVIL_HIT, 1.0f, 1.0f);
             }
         } else {
             if (hitResult.getType() == HitResult.Type.MISS) {
+
+                /* Get a list of all entities in a 32 block radius */
                 List<Entity> list = world.getOtherEntities(user, new Box(new BlockPos(hitResult.getPos())).expand(16));
-                System.out.println(list.size());
+
+                /* Attach a couple of compositions to those entities */
                 list.forEach(entity -> {
-//                    CompositionTracker.attach(new FloatAwayComposition(0.2f), entity);
+                    CompositionTracker.attach(new FloatAwayComposition(0.2f), entity);
                     CompositionTracker.attach(new SmokeComposition(), entity);
                 });
 
