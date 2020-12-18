9802bd6d2c7f8d8bb4791c6d6c8002e12e034d38
Ethan Johnson <ethan@ejohnsons.com>
2020-12-18 12:55:32 -0600

    Changed mixin location for generic compositions and hopefully fixed packet issue

--- src/main/java/dev/lazurite/thimble/mixin/WorldChunkMixin.java
+++ src/main/java/dev/lazurite/thimble/mixin/WorldChunkMixin.java
@@ -2,28 +2,28 @@ package dev.lazurite.thimble.mixin;
 
 import dev.lazurite.thimble.Thimble;
 import net.minecraft.entity.Entity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
- * This mixin class only modifies the {@link ServerWorld#spawnEntity} method
- * in order to make generic {@link dev.lazurite.thimble.composition.Composition}
+ * This mixin class only modifies the addEntity method in order to
+ * make generic {@link dev.lazurite.thimble.composition.Composition}
  * objects possible.
  * @author Ethan Johnson
  */
-@Mixin(ServerWorld.class)
-public class ServerWorldMixin {
+@Mixin(WorldChunk.class)
+public class WorldChunkMixin {
     /**
      * Stitches all generic {@link dev.lazurite.thimble.composition.Composition}
      * objects to an {@link Entity} when it spawns.
-     * @param entity the {@link Entity} to stitch {@link dev.lazurite.thimble.composition.Composition} objects to
+     * @param entity the {@link Entity} that is being added
      * @param info required by every mixin injection
      */
-    @Inject(method = "spawnEntity", at = @At("HEAD"))
-    public void spawnEntity(Entity entity, CallbackInfoReturnable<Boolean> info) {
+    @Inject(method = "addEntity", at = @At("HEAD"))
+    public void addEntity(Entity entity, CallbackInfo info) {
         Thimble.getStitches(entity.getClass()).forEach(factory -> Thimble.stitch(factory, entity));
     }
 }
